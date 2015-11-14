f7d041f44c05dd3247f48402ab14f9d4aaef459b
Daniel Clelland <daniel.clelland@gmail.com>
2015-11-14 19:15:54 +1300

    Created view controllers

---
+++ VOWL/VowelViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VowelViewController.swift
+//  VOWL
+//
+//  Created by Daniel Clelland on 14/11/15.
+//  Copyright © 2015 Daniel Clelland. All rights reserved.
+//
+
+import UIKit
+
+class VowelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
