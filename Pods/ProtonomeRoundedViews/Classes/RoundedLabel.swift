bab1b4dad91b3debc38e4b7125084257d88bcbcf
Daniel Clelland <daniel.clelland@gmail.com>
2016-05-08 20:16:59 +1200

    Updated RoundedViews to the version supporting text insets on the UILabel

--- Pods/ProtonomeRoundedViews/Classes/RoundedLabel.swift
+++ Pods/ProtonomeRoundedViews/Classes/RoundedLabel.swift
@@ -29,6 +29,34 @@ import UIKit
         }
     }
     
+    /// The label's top text inset.
+    @IBInspectable public var textInsetTop: CGFloat = 0.0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    /// The label's left text inset.
+    @IBInspectable public var textInsetLeft: CGFloat = 0.0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    /// The label's bottom text inset.
+    @IBInspectable public var textInsetBottom: CGFloat = 0.0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    /// The label's right text inset.
+    @IBInspectable public var textInsetRight: CGFloat = 0.0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
     // MARK: - Configuration
     
     private func configureView() {
@@ -38,16 +66,39 @@ import UIKit
     
     // MARK: - Overrides
     
+    override public func textRectForBounds(bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        let rect = super.textRectForBounds(textInsets.inset(bounds), limitedToNumberOfLines: numberOfLines)
+        return textInsets.inverse.inset(rect)
+    }
+    
     override public func drawTextInRect(rect: CGRect) {
         CGContextSetFillColorWithColor(UIGraphicsGetCurrentContext(), fillColor?.CGColor)
         backgroundPath.fill()
-        super.drawTextInRect(rect)
+        super.drawTextInRect(textInsets.inset(rect))
     }
     
     // MARK: - Private getters
 
     private var backgroundPath: UIBezierPath {
         return UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+    }
+    
+    private var textInsets: UIEdgeInsets {
+        return UIEdgeInsets(top: textInsetTop, left: textInsetLeft, bottom: textInsetBottom, right: textInsetRight)
+    }
+    
+}
+
+// MARK: - Private extensions
+
+private extension UIEdgeInsets {
+    
+    var inverse: UIEdgeInsets {
+        return UIEdgeInsets(top: -top, left: -left, bottom: -bottom, right: -right)
+    }
+    
+    func inset(rect: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(rect, self)
     }
     
 }
