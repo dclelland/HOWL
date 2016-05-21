717d3db6022aff01d4007779deb3adb471404a01
Daniel Clelland <daniel.clelland@gmail.com>
2016-05-21 14:40:48 +1200

    That's a bit nicer

--- HOWL/View Controllers/Phonemeboard/PhonemeboardView.swift
+++ HOWL/View Controllers/Phonemeboard/PhonemeboardView.swift
@@ -39,7 +39,7 @@ class PhonemeboardView: AudioPlot {
         let context = UIGraphicsGetCurrentContext()
         
         if (self.selected) {
-            CGContextSetFillColorWithColor(context, UIColor.protonome_blackColor().CGColor)
+            CGContextSetFillColorWithColor(context, trailPathColor.CGColor)
             trailPath.fill()
         }
     }
@@ -49,11 +49,17 @@ class PhonemeboardView: AudioPlot {
     private var trailPath: UIBezierPath {
         let path = UIBezierPath.makePath { make in
             trailLocations.enumerate().forEach { index, location in
-                make.oval(at: location, radius: CGFloat(trailLength - index) * 0.5)
+                let ratio = CGFloat(index).ilerp(min: 0.0, max: CGFloat(trailLength)).lerp(min: 1.0, max: 0.0)
+                let radius = pow(ratio, 2.0) * 12.0
+                make.oval(at: location, radius: radius)
             }
         }
         
         return path
+    }
+    
+    private var trailPathColor: UIColor {
+        return UIColor.protonome_lightColor(withHue: colorHue, saturation: colorSaturation)
     }
     
     private var trailLocation: CGPoint {
