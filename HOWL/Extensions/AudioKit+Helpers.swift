eb28cc7102f6cabf535c3690561f04d15a567ccf
Daniel Clelland <daniel.clelland@gmail.com>
2016-05-22 19:31:26 +1200

    Cleaned up audiokit helpers

--- HOWL/Extensions/AudioKit+Helpers.swift
+++ HOWL/Extensions/AudioKit+Helpers.swift
@@ -8,105 +8,52 @@
 
 import AudioKit
 
-func akp(num: Float) -> AKConstant {
-    return AKConstant(float: num)
+// MARK: Conversion
+
+protocol AKConvertible {
+    
+    var ak: AKConstant { get }
+
 }
 
-func akpi(num: Int) -> AKConstant {
-    return AKConstant(integer: num)
-}
+extension Int: AKConvertible {
 
-func akps(str: String) -> AKConstant {
-    return AKConstant(string: str)
-}
-
-func akpfn(file: String) -> AKConstant {
-    return AKConstant(filename: file)
-}
-
-extension Int {
-    var ak: AKConstant {return AKConstant(integer: self)}
-}
-
-extension Float {
-    var ak: AKConstant {return AKConstant(float: self)}
-    var midiratio: Float {return pow(2, self * 0.083333333333)}
-}
-
-extension Double {
-    var ak: AKConstant {return AKConstant(float: Float(self))}
-    var midiratio: Double {return pow(2, self * 0.083333333333)}
-}
-
-extension AKMultipleInputMathOperation {
-    convenience init(inputs: AKParameter...) {
-        self.init()
-        self.inputs = inputs
+    var ak: AKConstant {
+        return AKConstant(integer: self)
     }
+
 }
 
-// Arithmetic operators
+extension Float: AKConvertible {
 
-func + (left: AKParameter, right: AKParameter) -> AKParameter {
+    var ak: AKConstant {
+        return AKConstant(float: self)
+    }
+
+}
+
+extension Double: AKConvertible {
+
+    var ak: AKConstant {
+        return AKConstant(float: Float(self))
+    }
+
+}
+
+// MARK: Operators
+
+func +<T: AKParameter, U: AKParameter> (left: T, right: U) -> T {
     return left.plus(right)
 }
 
-func + (left: AKControl, right: AKControl) -> AKControl {
-    return left.plus(right)
-}
-
-func + (left: AKConstant, right: AKConstant) -> AKConstant {
-    return left.plus(right)
-}
-
-func + (left: AKAudio, right: AKAudio) -> AKAudio {
-    return left.plus(right)
-}
-
-func - (left: AKParameter, right: AKParameter) -> AKParameter {
+func -<T: AKParameter, U: AKParameter> (left: T, right: U) -> T {
     return left.minus(right)
 }
 
-func - (left: AKControl, right: AKControl) -> AKControl {
-    return left.minus(right)
-}
-
-func - (left: AKConstant, right: AKConstant) -> AKConstant {
-    return left.minus(right)
-}
-
-func - (left: AKAudio, right: AKAudio) -> AKAudio {
-    return left.minus(right)
-}
-
-func * (left: AKParameter, right: AKParameter) -> AKParameter {
+func *<T: AKParameter, U: AKParameter> (left: T, right: U) -> T {
     return left.scaledBy(right)
 }
 
-func * (left: AKControl, right: AKControl) -> AKControl {
-    return left.scaledBy(right)
-}
-
-func * (left: AKConstant, right: AKConstant) -> AKConstant {
-    return left.scaledBy(right)
-}
-
-func * (left: AKAudio, right: AKAudio) -> AKAudio {
-    return left.scaledBy(right)
-}
-
-func / (left: AKParameter, right: AKParameter) -> AKParameter {
-    return left.dividedBy(right)
-}
-
-func / (left: AKControl, right: AKControl) -> AKControl {
-    return left.dividedBy(right)
-}
-
-func / (left: AKConstant, right: AKConstant) -> AKConstant {
-    return left.dividedBy(right)
-}
-
-func / (left: AKAudio, right: AKAudio) -> AKAudio {
+func /<T: AKParameter, U: AKParameter> (left: T, right: U) -> T {
     return left.dividedBy(right)
 }
